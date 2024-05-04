fd45830fd7bf541e64060aa0d1c313e414acaeb7
ASUS <ASUS@AZIZDWIMUKTI>
2024-05-04 23:32:43 +0700

    menambahkan algoritma search dalam program

--- 0087_Doublelinkedlist/0087_Doublelinkedlist.cpp
+++ 0087_Doublelinkedlist/0087_Doublelinkedlist.cpp
@@ -61,7 +61,14 @@ void addNode() {
 }
 
 bool search() {
-
+	bool search(int rollNo, Node * *previous, Node * *current) {
+		*previous = NULL;
+		*current = START;
+		while (*current != NULL && (*current)->noMhs != rollNo) {
+		}		*previous = *current;
+		*current = (*current)->next;
+	}
+	return (*current != NULL);
 }
 
 
